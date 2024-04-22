1b82f2cd419fc64d7e9a9c48e16cd806addc0f39
brahi <b.munar@uniandes.edu.co>
2024-04-21 21:11:16 -0500

    database

--- src/models/event_model.py
+++ src/models/event_model.py
@@ -1,8 +1,10 @@
-from sqlalchemy import Column, String, Date
+from sqlalchemy import Column, String, Date, ForeignKey, Integer
 from src.database.base import Base
 from datetime import datetime, timezone
 from uuid import uuid4
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship, Mapped
+
 
 class EventModel(Base):
     __tablename__ = 'event'
@@ -13,3 +15,11 @@ class EventModel(Base):
     hora = Column(String(), nullable=False)
     description = Column(String())
     location = Column(String())
+    sportmen = relationship("Sportmen")
+
+class Sportmen(Base):
+    __tablename__ = 'sportmen'
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    event_id = Column(UUID(as_uuid=True), ForeignKey("event.id"), nullable=False)
+    sportmen = Column(UUID(as_uuid=True), nullable=False)
+    event = relationship("EventModel", back_populates="sportmen", lazy="joined")
